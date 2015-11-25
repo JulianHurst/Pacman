6504562e23f7e7f297c153e5136eb0b3eeb342ee
k3lkun <kelkunplay@gmail.com>
2015-11-25 09:51:37 +0100

    ajout de commentaire

--- collisions.cpp
+++ collisions.cpp
@@ -7,6 +7,12 @@ Collisions::Collisions()
 
 }
 
+/**
+ * @brief Collisions::colliding Gestion de la collision entre Pacman et les Fanômes
+ * @param obj1 Pointe vers Pacman
+ * @param obj2 Pointe vers un des fantômes
+ * @return
+ */
 bool Collisions::colliding(Pacman *obj1,Fantome *obj2){
     if(obj1->getgobj()->collidesWithItem(obj2->getgobj(),Qt::IntersectsItemBoundingRect)){
         obj1->CollisionFantome();
@@ -15,6 +21,11 @@ bool Collisions::colliding(Pacman *obj1,Fantome *obj2){
     return false;
 }
 
+/**
+ * @brief Collisions::colliding Gestion de la collision entre les personnages et le mur
+ * @param obj1 Pointe vers le personnage à tester (Pacman, Fantômes)
+ * @param obj2 Pointe vers le labyrinthe
+ */
 void Collisions::colliding(Personnage *obj1, Labyrinthe *obj2){    
     while(obj1->getgobj()->collidesWithItem(obj2->getgobj(),Qt::IntersectsItemBoundingRect))
         obj1->CollisionLab();
--- pacman.cpp
+++ pacman.cpp
@@ -2,17 +2,27 @@
 #include "fantome.h"
 #include <QDebug>
 
+/**
+ * @brief Pacman::Pacman constructeur par défaut de pacman
+ */
 Pacman::Pacman():
     Personnage()
 {
     gobj = new QGraphicsPixmapItem();
 }
 
+/**
+ * @brief Pacman::Pacman Constructeur avec paramètre de pacman
+ * @param x position de pacman sur la largeur
+ * @param y position de pacman sur la hauteur
+ * @param w taille de pacman en largeur
+ * @param h taille de pacman en hauteur
+ */
 Pacman::Pacman(float x,float y,double w,double h)
     :Personnage(x,y,w,h)
 {    
-    this->w=w*0.03;
-    this->h=0.062*h;
+    this->w=w*0.03; /*!< redimensionnement sur la largeur*/
+    this->h=0.062*h; /*!< redimensionnement sur la hauteur*/
     this->x=x-this->w/2;
     this->y+=0.08*h;
     this->lives=3;    
@@ -25,18 +35,35 @@ Pacman::Pacman(float x,float y,double w,double h)
     inity=this->y;
 }
 
+/**
+ * @brief Pacman::die réduit le nombre de vie de pacman
+ */
 void Pacman::die(){
     lives--;
 }
 
+/**
+ * @brief Pacman::getlives renvoie le nombre de vie courante
+ * @return
+ */
 int Pacman::getlives(){
     return lives;
 }
 
+/**
+ * @brief Pacman::setlives permet d'attribuer un nombre de vie
+ * @param lives
+ */
 void Pacman::setlives(int lives){
     this->lives=lives;
 }
 
+/**
+ * @brief Pacman::resize redimensionne le pacman
+ * @param w largeur de pacman
+ * @param h hauteur de pacman
+ * @return pacman redimensionné
+ */
 Pacman * Pacman::resize(int w,int h){
     Pacman *P=new Pacman(w/2,h/2+(0.15*h),w,h);
     P->setdir(this->dir);
